e480c4227953f57a51ec982c9b07be5032e683e2
Hendrix Peralta <hendrixperalta@gmail.com>
2023-05-17 20:26:06 +0900

    Prueba

---
+++ main.py
@@ -0,0 +1,52 @@
+# This is a sample Python script.
+
+class item():
+    def __init__(self, name, description, group, model, brand, external_code, quantity, location,
+                  group2=None, descr2=None, minimum=None, maximum=None, importance=None, seller=None, photo=None):
+        self.name = name
+        self.description = description
+        self.group = group
+        self.model = model
+        self.brand = brand
+        self.ext_code = external_code
+        self.quantity = quantity
+        self.location = location
+        self.group2 = group2
+        self.des2 = descr2
+        self.min = minimum
+        self.max = maximum
+        self.importance = importance
+        self.seller = seller
+        self.photo = photo
+
+    def add_quantity(self, quantity):
+        self.quantity = self.quantity + quantity
+
+    def substract_quantity(self, quantity):
+        self.quantity = self.quantity - quantity
+        if self.quantity < 0:
+            self.quantity = 0
+
+
+
+a = item("n", "d", "g", "m", "b", "c", 1, 2)
+print(a.quantity)
+#print(a.description)
+#print(a.model)
+#print(a.brand)
+#print(a.quantity)
+#print(a.location)
+
+b = item("n", "d", "g", "m","b", "c", 3, 2)
+print(b.quantity)
+#print(a.description)
+#print(a.model)
+#print(a.brand)
+#print(a.quantity)
+#print(a.location)
+
+a.add_quantity(4)
+print(a.quantity)
+
+a.substract_quantity(7)
+print(a.quantity)
