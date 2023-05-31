a0d8d49f1bec499712be223384338f9502503242
Hendrix Peralta <hendrixperalta@gmail.com>
2023-05-31 16:19:44 +0900

    connected the user input on main to the data transfer to the DB.
    - add_item function works for individual items as well as with random number of entries

--- menu.py
+++ menu.py
@@ -1,7 +1,75 @@
+import random
+import objects
+import DB
+
+
+def item_data_entry():
+    new_items = []
+    quantity_items = int(input("How many items you want to create: "))
+    if quantity_items == 1:
+        name = input("Name: ")
+        description = input("Description: ")
+        group = input("Group: ")
+        model = input("Model: ")
+        brand = input("Brand: ")
+        external_code = input("External Code: ")
+        quantity = input("Quantity: ")
+        location = input("Location: ")
+        group2 = input("Group 2: ")
+        des2 = input("Description 2: ")
+        minimum = input("Minimum: ")
+        maximum = input("maximum: ")
+        importance = input("Importance: ")
+        seller = input("Seller: ")
+        photo = input("Photo: ")
+
+        item = objects.item(name=name, description=description, group=group, model=model, brand=brand,
+                            external_code=external_code, quantity=quantity, location=location, seller=seller,
+                            group2=group2, des2=des2, minimum=minimum, maximum=maximum,
+                            importance=importance, photo=photo)
+
+        new_items.append(item)
+
+        print(new_items)
+
+        return new_items
+
+    elif quantity_items > 1:
+        print("n:", quantity_items)
+        print("range: ", range(quantity_items))
+        for i in range(quantity_items):
+            name = f"Item {i + 1}"
+            description = f"Description {i + 1}"
+            group = f"Group {random.randint(1, 10)}"
+            model = f"Model {i + 1}"
+            brand = f"Brand {i + 1}"
+            external_code = f"Location {random.randint(1, 40)}"
+            quantity = random.randint(50, 100)
+            location = f"Location {random.randint(1, 10)}"
+            seller = f"Seller {random.randint(1, 10)}"
+            group2 = f"Group2 {i + 1}"
+            des2 = f"Description2 {i + 1}"
+            minimum = random.randint(1, 10)
+            maximum = minimum * 10
+            importance = f"Importance {i + 1}"
+            photo = f"Photo {i + 1}"
+
+            items = objects.item(name=name, description=description, group=group, model=model, brand=brand,
+                                 external_code=external_code, quantity=quantity, location=location, seller=seller,
+                                 group2=group2, des2=des2, minimum=minimum, maximum=maximum,
+                                 importance=importance, photo=photo)
+            new_items.append(items)
+            # adding each item to the d
+            print("size of item", len(new_items))
+        return new_items
+
 
 def add_item():
     print("You selected option 1 - Add new item")
-    # Add your code here
+    new_items = item_data_entry()
+    print("size of item 2", len(new_items))
+    for item in new_items:
+        DB.add_item(item)
 
 
 def delete_item():
@@ -37,3 +105,36 @@ def delete_logs():
 def delete_database():
     print("You selected option 99 - Delete database")
     # Add your code here
+
+
+def trash():
+    new_items = []
+    for i in range(5):
+        name = f"Item {i + 1}"
+        description = f"Description {i + 1}"
+        group = f"Group {random.randint(1, 10)}"
+        model = f"Model {i + 1}"
+        brand = f"Brand {i + 1}"
+        external_code = f"Location {random.randint(1, 40)}"
+        quantity = random.randint(50, 100)
+        location = f"Location {random.randint(1, 10)}"
+        seller = f"Seller {random.randint(1, 10)}"
+        group2 = f"Group2 {i + 1}"
+        des2 = f"Description2 {i + 1}"
+        minimum = random.randint(1, 10)
+        maximum = minimum * 10
+        importance = f"Importance {i + 1}"
+        photo = f"Photo {i + 1}"
+
+        items = item(name=name, description=description, group=group, model=model, brand=brand,
+                     external_code=external_code, quantity=quantity, location=location, seller=seller,
+                     group2=group2, des2=des2, minimum=minimum, maximum=maximum,
+                     importance=importance, photo=photo)
+        new_items.append(items)
+
+    # adding each item to the db
+    for item in new_items:
+        DB.add_item(item)
+
+    for item in new_items:
+        DB.subtract_quantity(new_items[random.randint(0, len(new_items) - 1)], random.randint(10, 50))
